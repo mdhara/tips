0d6c55208e1a2ce5fe2dfd03353496ac7fa9d349
dhara <dhara.m44@gmail.com>
2016-03-20 17:39:29 -0700

    changing comment

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after  loading the view, typically from a nib.
         
         let defaults = NSUserDefaults.standardUserDefaults()
         var savedBillAmount = 0.00
